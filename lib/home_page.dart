cb9dcfd7bfdf4119dbbb831195c1bbb4925594b6
vivek <viveksharmapoudel@gmail.com>
2022-01-19 12:55:43 +0545

    fix: basics

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+
+    final double check =1;
+    final String name ="vivekd skjds sj";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Test App"),
+        ),
+        body: Center(
+          child: Container(
+            child: Text("$name $check"),
+          )
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
