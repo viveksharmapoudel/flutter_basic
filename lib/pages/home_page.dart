c8dd298630d6e6a1afb53751f1aeed507ad85a1d
vivek <viveksharmapoudel@gmail.com>
2022-01-24 12:06:24 +0545

    feat:gridview and gridtile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,7 +24,9 @@ class _HomePageState extends State<HomePage> {
     final productJson = await rootBundle.loadString("assets/files/data.json");
     final decodedData= jsonDecode(productJson); 
     var productData= decodedData["products"];
+    
     CategoryModal.products = List.from(productData).map<Product>((item) => Product.fromMap(item)).toList();
+    print(CategoryModal.products);
     setState(() {});
   }
 
@@ -33,17 +35,49 @@ class _HomePageState extends State<HomePage> {
   
     return Scaffold(
         appBar: AppBar(
-          title: Text("Test App"),
+          title: const Text("Test App"),
+          centerTitle: true,
         ),
-        body: (CategoryModal.products !=null && CategoryModal.products.isNotEmpty) ? ListView.builder(
-          itemCount: CategoryModal.products.length,
-          itemBuilder:(context, index){
-            return ProductWidget(
-              product: CategoryModal.products[index],
+        body: (CategoryModal.products.isNotEmpty) ?
+          GridView.builder(
+            gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2
+              ), 
+            itemCount: CategoryModal.products.length,
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 8.0),
+            itemBuilder: (context, index){
+              final item= CategoryModal.products[index];
+              return Card(
+                color: Colors.grey.shade100,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0)),
+                margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child:  GridTile(
+                  header: Container(
+                    // ignore: prefer_const_constructors
+                    decoration:  BoxDecoration(
+                        color: Colors.purple,
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15),
+                        )
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 0),
+                    child: Text(item.name,
+                    textAlign: TextAlign.center,
+                
+                    style: const TextStyle(
+                      color: Colors.white,
+          
+                    ),
+                    )),
+                  footer: Text('\$ ${item.price.toString()}'),
+                  child: Image.network(item.image)
+                ),
               );
-          },
-        )
-        : Center(
+            }
+          )
+        : const Center(
           child: CircularProgressIndicator(),
         )
         ,
@@ -51,3 +85,13 @@ class _HomePageState extends State<HomePage> {
       );
   }
 }
+
+
+//  ListView.builder(
+//           itemCount: CategoryModal.products.length,
+          // itemBuilder:(context, index){
+          //   return ProductWidget(
+          //     product: CategoryModal.products[index],
+          //     );
+          // },
+//         )
